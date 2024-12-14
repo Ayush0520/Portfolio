9ffda35d15bcc98a1e3d92eef6c4a01d7a49c755
ayushsingh-gep <ayush20133@iiitd.ac.in>
2024-12-14 10:37:08 +0530

    Config update

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  assetsInclude: ['**/*.heic'],
   plugins: [
     react(),
   ],
+  base: '/Portfolio/',
 });
